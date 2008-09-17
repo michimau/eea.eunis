2c287502487b775dd285d0d979fa79623436423f
Risto Alt <risto.alt@gmail.com>
2008-09-17 11:12:07 +0000

    Ticket #916

--- src/script/utils.js
+++ src/script/utils.js
@@ -2,7 +2,7 @@ var errInvalidCountry = country_invalid_msg;
 
 function openTSVDownload( URL )
 {
-  open( URL, "", "scrollbars=yes,toolbar=0,resizable=yes, location=0,width=450,height=280,left=490,top=0");
+  window.open( encodeURI(URL), "", "scrollbars=yes,toolbar=0,resizable=yes, location=0,width=450,height=280,left=490,top=0");
 }
 
 //

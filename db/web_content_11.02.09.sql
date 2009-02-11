3932379a8027e2bdbbeb6c6416da02e029c69d48
Risto Alt <risto.alt@gmail.com>
2009-02-11 14:33:55 +0000

    Bug fix

--- db/web_content_11.02.09.sql
+++ db/web_content_11.02.09.sql
@@ -1,2 +1,2 @@
-INSERT INTO eunis_web_content (ID_PAGE, CONTENT, CONTENT_VALID) VALUES ('services_refresh_title', 'Refresh template', 1);
-INSERT INTO eunis_web_content (ID_PAGE, CONTENT, CONTENT_VALID) VALUES ('refresh_language', 'Refresh template', 1);
+INSERT INTO eunis_web_content (ID_PAGE, CONTENT, CONTENT_VALID) VALUES ('services_refresh_template', 'Refresh template', 1);
+INSERT INTO eunis_web_content (ID_PAGE, CONTENT, CONTENT_VALID) VALUES ('refresh_template', 'Refresh template', 1);

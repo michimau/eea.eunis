7bad3ad6da3a94416fd6d2cd8e01fa1d7ce3e970
Søren Roug <soren.roug@eea.europa.eu>
2009-03-13 10:55:04 +0000

    Locate the templates relative to the INSTANCE_HOME

--- src/classes/ro/finsiel/eunis/admin/UpdateTemplate.java
+++ src/classes/ro/finsiel/eunis/admin/UpdateTemplate.java
@@ -14,55 +14,54 @@ import javax.servlet.http.HttpServletResponse;
 
 import ro.finsiel.eunis.WebContentManagement;
 
+
 public class UpdateTemplate extends HttpServlet {
 	
-	/*
+    /*
      *  (non-Javadoc)
      * @see javax.servlet.http.HttpServlet#service(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
     public void service(HttpServletRequest req, HttpServletResponse res)
-                                            throws ServletException, IOException {
-        
-          
-       	WebContentManagement wm = new WebContentManagement();
+        throws ServletException, IOException {
+
+        WebContentManagement wm = new WebContentManagement();
        	
-       	String baseDir = getServletContext().getInitParameter("TOMCAT_HOME");
+        String baseDir = getServletContext().getInitParameter("INSTANCE_HOME");
        	
-       	String headerUrl = getServletContext().getInitParameter("TEMPLATES_HEADER");
-       	String headerText = wm.readContentFromURL(headerUrl);
-       	String headerFileName = baseDir + getServletContext().getInitParameter("TEMPLATE_HEADER_LOCATION");
+        String headerUrl = getServletContext().getInitParameter("TEMPLATES_HEADER");
+        String headerText = wm.readContentFromURL(headerUrl);
+        String headerFileName = baseDir + getServletContext().getInitParameter("TEMPLATE_HEADER_LOCATION");
        	
-       	String reqHeaderUrl = getServletContext().getInitParameter("TEMPLATES_REQUIRED_HEAD");
-       	String reqHeaderText = wm.readContentFromURL(reqHeaderUrl);
-       	String reqHeaderFileName = baseDir + getServletContext().getInitParameter("TEMPLATE_REQUIRED_HEAD_LOCATION");
+        String reqHeaderUrl = getServletContext().getInitParameter("TEMPLATES_REQUIRED_HEAD");
+        String reqHeaderText = wm.readContentFromURL(reqHeaderUrl);
+        String reqHeaderFileName = baseDir + getServletContext().getInitParameter("TEMPLATE_REQUIRED_HEAD_LOCATION");
        	
-       	String footerUrl = getServletContext().getInitParameter("TEMPLATES_FOOTER");
-       	String footerText = wm.readContentFromURL(footerUrl);
-       	String footerFileName = baseDir + getServletContext().getInitParameter("TEMPLATE_FOOTER_LOCATION");
+        String footerUrl = getServletContext().getInitParameter("TEMPLATES_FOOTER");
+        String footerText = wm.readContentFromURL(footerUrl);
+        String footerFileName = baseDir + getServletContext().getInitParameter("TEMPLATE_FOOTER_LOCATION");
        	
-       	writeFile(headerFileName, headerText);
-       	writeFile(reqHeaderFileName, reqHeaderText);
-       	writeFile(footerFileName, footerText);       	
-        
+        writeFile(headerFileName, headerText);
+        writeFile(reqHeaderFileName, reqHeaderText);
+        writeFile(footerFileName, footerText);       	
+
         res.sendRedirect("templateUpdated.jsp");
-        
     }
-    
+
     private void writeFile(String fileName, String txt) throws ServletException, IOException {
-		
-    	
-    	try{
-    		FileOutputStream fos = new FileOutputStream(fileName);
-    		fos.write(new String("<%@page contentType=\"text/html;charset=UTF-8\"%>\n").getBytes());
-    		fos.write(txt.getBytes("UTF-8"));
+
+        try {
+            FileOutputStream fos = new FileOutputStream(fileName);
+
+            fos.write(new String("<%@page contentType=\"text/html;charset=UTF-8\"%>\n").getBytes());
+            fos.write(txt.getBytes("UTF-8"));
 			
-			if (fos != null) {
-				fos.flush();
-				fos.close();
-			}
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
-	}
-    
+            if (fos != null) {
+                fos.flush();
+                fos.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }

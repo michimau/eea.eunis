abc5c8d726de324a3c72d1f6dccfadd16a1ae55c
Søren Roug <soren.roug@eea.europa.eu>
2011-07-15 08:10:56 +0000

    The class path was built incorrectly.

--- web/src/main/scripts/postimport.sh
+++ web/src/main/scripts/postimport.sh
@@ -6,11 +6,11 @@ cd $eunis/classes
 java=/usr/bin/java
 
 libpath=$eunis/lib
-cp=$cp:$libpath/log4j-1.2.13.jar
-cp=$cp:$CLASSPATH
 
 # !!!!!!!!!!!!!!!!! CHECK, if mysql JAR is correct !!!!!!!!!!!!!!
 cp=@MYSQL.JAR@
+cp=$cp:$libpath/log4j-1.2.13.jar:$libpath/jrf-ver.unknown.jar
+cp=$cp:$CLASSPATH
 
 if [ "$1" = "" ]; then
 	echo "Missing argument! Possible first arguments are: sites, empty_digir, digir, statistics, species_tab, sites_tab, habitats_tab, taxonomy_tree"

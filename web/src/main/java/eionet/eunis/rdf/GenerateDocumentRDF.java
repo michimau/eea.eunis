2877d01dfd6ad3e5f7494cfa430084a450edfd7c
Søren Roug <soren.roug@eea.europa.eu>
2011-08-22 15:10:32 +0000

    Setting the xml:base property

--- web/src/main/java/eionet/eunis/rdf/GenerateDocumentRDF.java
+++ web/src/main/java/eionet/eunis/rdf/GenerateDocumentRDF.java
@@ -8,15 +8,21 @@ import eionet.eunis.dto.DcIndexDTO;
 
 public class GenerateDocumentRDF {
 
+    /**
+     * RDF root element. Domain name is hardwired. It should have come from getContext().getInitParameter("DOMAIN_NAME")
+     * or have been provided in constructor.
+     */
     public static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-        + "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n"
-        + "xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n"
-        + "xmlns=\"http://purl.org/dc/terms/\">\n";
+        + "<rdf:RDF xml:base=\"http://eunis.eea.europa.eu/documents/\"\n"
+        + "  xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n"
+        + "  xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n"
+        + "  xmlns=\"http://purl.org/dc/terms/\">\n";
 
     private DcIndexDTO object;
     private List<AttributeDto> attributes;
+    /** Identifier of the document (ID_DC). */
     private String id;
-    private StringBuffer rdf;
+    //private StringBuffer rdf;
 
     public GenerateDocumentRDF(String id) {
         if (id != null && id.length() > 0) {
@@ -24,7 +30,7 @@ public class GenerateDocumentRDF {
             this.attributes = DaoFactory.getDaoFactory().getDocumentsDao().getDcAttributes(id);
         }
         this.id = id;
-        this.rdf = new StringBuffer();
+        //this.rdf = new StringBuffer();
     }
 
     public GenerateDocumentRDF(DcIndexDTO object) {
@@ -33,10 +39,11 @@ public class GenerateDocumentRDF {
             this.attributes = DaoFactory.getDaoFactory().getDocumentsDao().getDcAttributes(object.getIdDc());
             this.id = object.getIdDc();
         }
-        this.rdf = new StringBuffer();
+        //this.rdf = new StringBuffer();
     }
 
     public StringBuffer getDocumentRdf() {
+        StringBuffer rdf = new StringBuffer();
         try {
             if (object != null && attributes != null) {
                 rdf.append("<rdf:Description rdf:about=\"http://eunis.eea.europa.eu/documents/").append(id).append("\">\n");
@@ -44,7 +51,7 @@ public class GenerateDocumentRDF {
                 rdf.append(RDFUtil.writeLiteral("title", object.getTitle()));
                 rdf.append(RDFUtil.writeLiteral("alternative", object.getAlternative()));
                 rdf.append(RDFUtil.writeLiteral("creator", object.getSource()));
-                rdf.append(RDFUtil.writeReference("source", object.getUrl()));
+                rdf.append(RDFUtil.writeReference("rdfs:seeAlso", object.getUrl()));
                 rdf.append(RDFUtil.writeLiteral("contributor", object.getEditor()));
                 rdf.append(RDFUtil.writeLiteral("created", object.getCreated()));
                 rdf.append(RDFUtil.writeLiteral("publisher", object.getPublisher()));

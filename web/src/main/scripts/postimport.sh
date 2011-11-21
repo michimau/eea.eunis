096d11f1ae9ea672b0a76dbe80f94ad604765b59
Risto Alt <risto.alt@gmail.com>
2011-11-21 15:44:33 +0000

    Ticket #1194 - Use token replacement in maven build

--- web/src/main/scripts/postimport.sh
+++ web/src/main/scripts/postimport.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# !!!!!!!!!!!!!!!!!! ADJUST THESE !!!!!!!!!!!!!!!!!!
 eunis=@WEBAPP.HOME@/WEB-INF
 cd $eunis/classes
 java=/usr/bin/java
 
-# !!!!!!!!!!!!!!!!! CHECK, if mysql JAR is correct !!!!!!!!!!!!!!
 cp=@MYSQL.JAR@
 cp=$cp:@LOG4J.JAR@
 cp=$cp:@JRF.JAR@

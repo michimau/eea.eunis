12d15ffb03cf2c002b801af2b10a8ed8264e239c
Søren Roug <soren.roug@eea.europa.eu>
2013-07-28 08:56:04 +0000

    190 new simple tests.

--- web/src/test/java/ro/finsiel/eunis/VarargsTest.java
+++ web/src/test/java/ro/finsiel/eunis/VarargsTest.java
@@ -1,28 +1,33 @@
 package ro.finsiel.eunis;
-import junit.framework.TestCase;
 
-public class VarargsTest extends TestCase {
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+public class VarargsTest {
 	
+        @Test
 	public void test_varargs() {
 		String value = varargMethod("param1", "param2");
 		assertEquals("test", value);
 	}
 	
+        @Test
 	public void test_phrase_string() {
 		String value = cmsPhraseMethod("page", "param1");
 		assertEquals("test", value);
 	}
 	
+        @Test
 	public void test_phrase_int() {
 		String value = cmsPhraseMethod("page", 42);
 		assertEquals("test", value);
 	}
 	
-        private String cmsPhraseMethod( String idPage, Object... arguments ) {
+        private String cmsPhraseMethod(String idPage, Object... arguments) {
 		return "test";
         }
 
-	private String varargMethod( Object... arguments ){
+	private String varargMethod(Object... arguments) {
 		return "test";
 	}
 }
